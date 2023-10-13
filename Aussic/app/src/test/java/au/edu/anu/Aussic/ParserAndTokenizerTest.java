7e9c468683623154898a9595555b225d081c020a
Oscar <u7581818@anu.edu.au>
2023-10-13 18:29:37 +1100

    implemented parser and tokenizer test

--- Aussic/app/src/test/java/au/edu/anu/Aussic/ParserAndTokenizerTest.java
+++ Aussic/app/src/test/java/au/edu/anu/Aussic/ParserAndTokenizerTest.java
@@ -1,48 +1,83 @@
-//package au.edu.anu.Aussic.models.ParseTest;
-//
-//import org.junit.Test;
-//
-//
-//import java.util.List;
-//
-//import static junit.framework.TestCase.assertEquals;
-////import static org.junit.Assert.assertTrue;
-////import static org.junit.jupiter.api.Assertions.*;
-//
-//public class ParserAndTokenizerTest {
-//
-//    @Test
-//    public void testTokenizer() {
-//        Tokenizer tokenizer = new Tokenizer();
-//        List<Token> tokens = tokenizer.tokenize("ARTIST_NAME:Beatles ALBUM_NAME:Abbey Road");
-//
-//        assertEquals(2, tokens.size());
-//
-//        //assertTrue(tokens.get(0) instanceof FieldToken);
-//        assertEquals("ARTIST_NAME", (tokens.get(0)).getType());
-//        assertEquals("Beatles", tokens.get(0).getValue());
-//
-//        //assertTrue(tokens.get(1) instanceof FieldToken);
-//        assertEquals("ALBUM_NAME", (tokens.get(1)).getType());
-//        assertEquals("Abbey Road", tokens.get(1).getValue());
-//    }
-//
-//    @Test
-//    public void testParser() {
-//        Tokenizer tokenizer = new Tokenizer();
-//        List<Token> tokens = tokenizer.tokenize("ARTIST_NAME:Beatles ALBUM_NAME:Abbey Road");
-//        System.out.println("tokens:" + tokens);
-//        Parser parser = new Parser(tokens);
-//
-//
-//        QueryNode queryNode = parser.parse();
-//
-//        assertEquals(2, queryNode.conditions.size());
-//
-//        assertEquals("ARTIST_NAME", queryNode.conditions.get(0).type);
-//        assertEquals("Beatles", queryNode.conditions.get(0).value);
-//
-//        assertEquals("ALBUM_NAME", queryNode.conditions.get(1).type);
-//        assertEquals("Abbey Road", queryNode.conditions.get(1).value);
-//    }
-//}
+package au.edu.anu.Aussic;
+
+import org.junit.Test;
+import au.edu.anu.Aussic.models.parserAndTokenizer.Token;
+import au.edu.anu.Aussic.models.parserAndTokenizer.Tokenizer;
+import au.edu.anu.Aussic.models.parserAndTokenizer.Parser;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+
+public class ParserAndTokenizerTest {
+
+    @Test
+    public void testTokenCreation() {
+        Token token = new Token("\\a",Token.Type.ARTISTNAME);
+        assertEquals("\\a", token.getToken());
+        assertEquals(Token.Type.ARTISTNAME, token.getType());
+    }
+
+    @Test
+    public void testTokenization() {
+        Tokenizer tokenizer = new Tokenizer("\\a Tina Arena; \\n I Want to Know What Love Is (Single Edit)");
+
+        assertTrue(tokenizer.hasNext());
+        assertEquals(Token.Type.ARTISTNAME, tokenizer.current().getType());
+        tokenizer.next();
+
+        assertTrue(tokenizer.hasNext());
+        assertEquals("Tina Arena",tokenizer.current().getToken());
+        tokenizer.next();
+
+        assertTrue(tokenizer.hasNext());
+        assertEquals(Token.Type.SEMICOLON, tokenizer.current().getType());
+        tokenizer.next();
+
+        assertTrue(tokenizer.hasNext());
+        assertEquals(Token.Type.NAME, tokenizer.current().getType());
+        tokenizer.next();
+
+        assertTrue(tokenizer.hasNext());
+        assertEquals("I Want to Know What Love Is (Single Edit)",tokenizer.current().getToken());
+        tokenizer.next();
+
+        assertFalse(tokenizer.hasNext());
+    }
+
+    @Test
+    public void testSimpleParsing() {
+        Parser parser = new Parser(new Tokenizer("\\a Tina Arena; \\n I Want to Know What Love Is (Single Edit)"));
+        Map<String, String> result = parser.Parse();
+        assertEquals("Tina Arena", result.get("artistName"));
+        assertEquals("I Want to Know What Love Is (Single Edit)", result.get("name"));
+    }
+
+    @Test
+    public void testAdvancedParsing() {
+        Parser parser = new Parser(new Tokenizer("\\n Walk the Line; \\a Iggy Azalea; \\r 2014-01-01; \\g [\"Hip-Hop/Rap\", \"Music\",  \"Alternative Rap\",  \"Rap\", \"Underground Rap\"]"));
+        Map<String, String> result = parser.Parse();
+        assertEquals("Iggy Azalea", result.get("artistName"));
+        assertEquals("Walk the Line", result.get("name"));
+        assertEquals("2014-01-01", result.get("releaseDate"));
+        assertEquals("[\"Hip-Hop/Rap\", \"Music\",  \"Alternative Rap\",  \"Rap\", \"Underground Rap\"]", result.get("genre"));
+    }
+
+}
+
+
+
+
+//            "genreNames": [
+//            "Hip-Hop/Rap",
+//            "Music",
+//            "Alternative Rap",
+//            "Rap",
+//            "Underground Rap"
+//            ],
+//            "releaseDate": "2014-01-01",
+//            "name": "Walk the Line",
+//            "artistName": "Iggy Azalea"
+
